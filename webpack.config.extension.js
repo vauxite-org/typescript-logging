998ac08bf14769c242ba109edb7053086b868694
mreuvers <martijn.reuvers@gmail.com>
2016-10-02 10:07:49 +0200

    * Add react stuff, to use for extension later. * Add webpack config for that (untested yet)

---
+++ webpack.config.extension.js
@@ -0,0 +1,59 @@
+var webpack = require('webpack');
+var path = require('path');
+var CommonsChunkPlugin = require("webpack/lib/optimize/CommonsChunkPlugin");
+
+var BUILD_BUNDLE_DIR = './dist/extension/browser/chrome/js';
+var APP_SRC_DIR = './src/extension/browser/core';
+
+var config = {
+
+  entry: {
+
+    vendor: ["react","react-dom", "react-router"],
+    index: [APP_SRC_DIR + '/index.tsx'],
+  },
+
+
+   output: {
+     path: BUILD_BUNDLE_DIR,
+     filename: "[name].react.js"
+   },
+
+
+  // Enable sourcemaps for debugging webpack's output.
+  devtool: "source-map",
+
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
+  },
+
+  module: {
+    loaders: [
+      // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
+      { test: /\.tsx?$/, loaders: ["ts-loader"] }
+    ],
+
+    preLoaders: [
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      { test: /\.js$/, loader: "source-map-loader" }
+    ]
+  },
+
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new CommonsChunkPlugin({
+      name: "vendor",
+      minChunks: Infinity
+    })
+  ],
+
+};
+
+module.exports = config;
