b377fd37c83c94bbf4b53318204cd9b5ce1b5311
martijn <martijnr.work@gmail.com>
2020-08-21 13:06:30 +0200

    Refers issue #58
    Minimal skeleton, which can compile, test & build.
    - Latest and greatest typescript (4.0.2)
    - Targeting es6, not going to bother with outdated browsers.
    - Uses rollup for bundling, we will ship both commonjs and esm modules in the future.
    - Uses jest for testing.
    - Strict tsconfig
    - Enabled tslint recommended, with some changes.
    - Adds apache 2 license, and initial README.MD

---
+++ core/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "roots": [
+    "src/typescript",
+  ],
+  "preset": "ts-jest",
+  "moduleFileExtensions": [
+    "ts",
+    "tsx",
+    "js",
+    "jsx",
+    "json",
+    "node",
+  ],
+};
---
+++ core/rollup.config.js
@@ -0,0 +1,33 @@
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "rollup-plugin-typescript2";
+import resolve from "@rollup/plugin-node-resolve";
+
+import pkg from "./package.json";
+
+export default {
+  input: "src/typescript/main/typescript-logging-core.ts",
+  output: [
+    {
+      file: pkg.main,
+      format: "cjs",
+      sourcemap: true,
+    },
+    {
+      file: pkg.module,
+      format: "esm",
+      sourcemap: true,
+    },
+  ],
+
+  plugins: [
+    typescript({
+      typescript: require("typescript"),
+      clean: true,
+    }),
+    commonjs(),
+    // Allow node_modules resolution, so you can use 'external' to control
+    // which external modules to include in the bundle
+    // https://github.com/rollup/rollup-plugin-node-resolve#usage
+    resolve(),
+  ],
+};
