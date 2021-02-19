246c8191ef5abb092f71be41ca6fbaafffaece0f
martijn <mreuvers@notexistingemail.com>
2021-02-19 13:59:38 +0100

    Add TypeUtils, adds a PartialExcept which allows us to make all props optional except ...
    Added a compute function to the EnhancedMap.

--- core/src/typescript/main/util/EnhancedMap.ts
+++ core/src/typescript/main/util/EnhancedMap.ts
@@ -9,13 +9,34 @@ export class EnhancedMap<K,V> extends Map<K, V> {
    * The value is then stored for given key and returned.
    * @param key Key
    * @param computer Computer which is called only if key has no mapping yet.
+   * @return Existing value if the key already existed, or the newly computed value.
    */
-  public computeIfAbsent(key: K, computer: (currentKey: K) => V): V | undefined {
+  public computeIfAbsent(key: K, computer: (currentKey: K) => V): V {
     if (this.has(key)) {
-      return this.get(key);
+      return this.get(key) as V;
     }
     const newValue = computer(key);
     this.set(key, newValue);
+    return newValue;
+  }
+
+  /**
+   * Computes a value for given key, the computer can return a value V (in which case the map
+   * will set the value for given key), if it returns undefined the mapping for key K will be
+   * removed.
+   * @param key Key to compute
+   * @param computer Computer which is called, note that the currentValue argument contains the existing
+   *                 value or is undefined when no mapping exists for the key.
+   */
+  public compute(key: K, computer: (currentKey: K, currentValue: V | undefined) => V | undefined) {
+    const currentValue = this.get(key);
+    const newValue = computer(key, currentValue);
+    if (newValue) {
+      this.set(key, newValue);
+    }
+    else {
+      this.delete(key);
+    }
     return newValue;
   }
 }
