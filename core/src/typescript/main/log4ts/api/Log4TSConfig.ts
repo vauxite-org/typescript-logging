fc0d238471ac647a168766ff00f3de2c1f4cb1c9
martijn <mreuvers@notexistingemail.com>
2021-02-23 17:24:42 +0100

    Simplify the Log4TSConfig and Log4TSGroupConfig settings by extending them from the core LoggerSettings.
    Also drop "channel" for groups, I don't see a good
    use case for this yet.

--- core/src/typescript/main/log4ts/api/Log4TSConfig.ts
+++ core/src/typescript/main/log4ts/api/Log4TSConfig.ts
@@ -1,23 +1,13 @@
-import {ArgumentFormatterType, DateFormatterType, LogLevel, MessageArgumentFormatterType} from "../../core";
+import {LogSettings} from "../../core";
 import {Log4TSGroupConfig, Log4TSGroupConfigOptional} from "./Log4TSGroupConfig";
 
 /**
  * Configuration for the log4ts flavor (used by Log4TSProvider).
  */
-export interface Log4TSConfig {
-  readonly level: LogLevel;
+export interface Log4TSConfig extends LogSettings {
 
   /** Groups registered, a group is used to match loggers by name/expression path, at least 1 group is required */
   readonly groups: ReadonlyArray<Log4TSGroupConfig>;
-
-  /** The date formatter in use */
-  readonly dateFormatter: DateFormatterType;
-
-  /** The argument formatter in use */
-  readonly argumentFormatter: ArgumentFormatterType;
-
-  /** The message argument formatter in use */
-  readonly messageArgumentFormatter: MessageArgumentFormatterType;
 }
 
 /**
@@ -25,7 +15,7 @@ export interface Log4TSConfig {
  * All is optional, except groups of which 1 is required. Defaults are used when
  * not specified.
  */
-export type Log4TSConfigOptional = Partial<Omit<Log4TSConfig, "groups">> & {
+export type Log4TSConfigOptional = Partial<LogSettings> & {
   /**
    * Configure at least 1 group.
    */
