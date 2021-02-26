9d6bf49741bd693193820d473755abd066aca388
martijn <mreuvers@notexistingemail.com>
2021-02-26 09:33:15 +0100

    Put channel back on the Log4TSGroupConfig, but remove it from from the optional one. This allows us to see that the correct channel is present but won't allow to configure it on a group for now.

--- core/src/typescript/main/log4ts/api/Log4TSGroupConfig.ts
+++ core/src/typescript/main/log4ts/api/Log4TSGroupConfig.ts
@@ -4,7 +4,7 @@ import {PartialExcept} from "../../util/TypeUtils";
 /**
  * Group config for a Log4TSProvider.
  */
-export type Log4TSGroupConfig = Omit<Log4TSConfig, "groups" | "channel"> & {
+export type Log4TSGroupConfig = Omit<Log4TSConfig, "groups"> & {
   readonly expression: RegExp;
 }
 
@@ -13,4 +13,4 @@ export type Log4TSGroupConfig = Omit<Log4TSConfig, "groups" | "channel"> & {
  * All other options are optional and can be omitted, in which case they use
  * the defaults as specified for the Log4TSProvider this group belongs to.
  */
-export type Log4TSGroupConfigOptional = PartialExcept<Log4TSGroupConfig, "expression">;
+export type Log4TSGroupConfigOptional = PartialExcept<Log4TSGroupConfig, "expression" | "channel">;
