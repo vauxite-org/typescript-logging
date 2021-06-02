6398a2d4d9081d3150513b7d23c1c118d58b0c9a
martijn <mreuvers@notexistingemail.com>
2021-06-02 18:09:24 +0200

    Issue #75: Add Log4TSControl and Log4TSControlProvider interfaces
    To respectively access a provider, and control the provider.

---
+++ core/src/typescript/main/log4ts/api/Log4TSControlProvider.ts
@@ -0,0 +1,36 @@
+/**
+ * Provides access to the current Log4TSProvider to dynamically change its log level for one or more groups.
+ */
+export interface Log4TSControlProvider {
+  /**
+   * Shows current settings.
+   */
+  readonly showSettings: () => void;
+
+  /**
+   * Update given group or all groups to given log level.
+   */
+  readonly update: (level: Log4TSControlProviderLogLevel, groupId?: number | string) => void;
+
+  /**
+   * Resets the levels of the groups to exactly when this Log4TSControlProvider was initially created (by Log4TSControl.getProvider(..)).
+   */
+  readonly reset: () => void;
+
+  /**
+   * Saves the current levels of the groups to the localStorage if available (otherwise does nothing).
+   */
+  readonly save: () => void;
+
+  /**
+   * Restores the log levels to the saved state if they were saved, otherwise does nothing.
+   */
+  readonly restore: (logRestoreFailures?: boolean) => void;
+
+  /**
+   * Shows help about this Log4TSControlProvider.
+   */
+  readonly help: () => void;
+}
+
+export type Log4TSControlProviderLogLevel = "trace" | "debug" | "info" | "warn" | "error" | "fatal";
