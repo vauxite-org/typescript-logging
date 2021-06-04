040127173f617afbe40d0f24531a2d4b9d0d460c
martijn <mreuvers@notexistingemail.com>
2021-06-04 12:09:47 +0200

    Issue #75: Export the Log4TS api for public access

--- core/src/typescript/main/log4ts/index.ts
+++ core/src/typescript/main/log4ts/index.ts
@@ -1,3 +1,8 @@
 export {Log4TSConfig, Log4TSConfigOptional} from "./api/Log4TSConfig";
+export {Log4TSControl} from "./api/Log4TSControl";
+export {Log4TSControlProvider} from "./api/Log4TSControlProvider";
 export {Log4TSProvider} from "./api/Log4TSProvider";
 export {Log4TSGroupConfig, Log4TSGroupConfigOptional} from "./api/Log4TSGroupConfig";
+
+/* Exports from impl to provide Log4TSControl access */
+export {LOG4TS_LOG_CONTROL} from "./impl/Log4TSProviderService";
