8d06d55f5093a9fa6a52560256d512bf3b2c6da9
martijn <mreuvers@users.noreply.github.com>
2021-10-22 15:47:20 +0200

    Issue #84: Simplify Logger interface
    Users can now log messages, errors and optional arguments - the latter
    using a rest parameter.
    
    Make the arguments unknown as that is what they really are.
    
    Drop the ArgumentsType type.

--- core/src/typescript/main/core/api/type/ArgumentsType.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Arguments of a log message, either must be given as readonly array
- * or as function returning a readonly array.
- */
-export type ArgumentsType = ReadonlyArray<any> | (() => ReadonlyArray<any>);
