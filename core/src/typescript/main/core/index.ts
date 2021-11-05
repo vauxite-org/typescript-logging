5a6edc529e4817c482c5462c2f5846ebf75196d3
martijn <mreuvers@users.noreply.github.com>
2021-11-05 10:30:54 +0100

    For future reference, it's unclear on how exactly we want to split core and the actual implementation modules. Make typescript-logging into typescript-logging-core.
    So we do all imports from typescript-logging-core (in case we do want
    to publish it separately on npm later) for depending modules (log4ts, category).
    
    The current setup is a mix between workspaces and ordinary npm link. Not
    the prettiest, but good enough until some final decision
    is made in the future.
    
    Add/update various core config files.

--- core/src/typescript/main/core/index.ts
+++ core/src/typescript/main/core/index.ts
@@ -1,6 +1,3 @@
-/* This one must be first to assure the internal logging is available before we export anything else */
-export * as $internal from "../internal/InternalLogger";
-
 import {LogConfig} from "./api/config/LogConfig";
 import {LogProvider} from "./api/LogProvider";
 import {LogProviderImpl} from "./impl/LogProviderImpl";
@@ -29,6 +26,7 @@ export {LoggerNameType} from "./api/type/LoggerNameType";
 export {LogMessageType} from "./api/type/LogMessageType";
 export {MessageArgumentFormatterType} from "./api/type/MessageArgumentFormatterType";
 export {MessageFormatterType} from "./api/type/MessageFormatterType";
+export * from "./api/type/TypeUtils";
 
 /* Export default channels and a few utilities from impl, only what we deem convenient for end users */
 export * from "./impl/channel/DefaultChannels";
@@ -38,8 +36,6 @@ export * from "./impl/DefaultFormatters";
  * Create a new LogProvider, this is for flavor usage only. End users should not
  * use this and instead use whatever the flavor offers to build some config and
  * get loggers from there.
- *
- * @internal
  */
 export function createLogProvider(name: string, settings: LogConfig): LogProvider {
   return new LogProviderImpl(name, settings);
