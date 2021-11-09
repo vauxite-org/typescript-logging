48cdd10043f74e3526d9a0cae7f619c1dcf1235f
martijn <mreuvers@users.noreply.github.com>
2021-11-09 17:21:57 +0100

    Simplify config for jest and rollup, to have base configs. Both core and log4ts style use it.

--- core/rollup.config.js
+++ core/rollup.config.js
@@ -1,13 +1,10 @@
-import commonjs from "@rollup/plugin-commonjs";
-import typescript from "rollup-plugin-typescript2";
-import resolve from "@rollup/plugin-node-resolve";
+const sharedConfig = require("../rollup.config.base");
 
 import pkg from "./package.json";
 
-export default {
+module.exports = {
+  ...sharedConfig,
   input: "src/typescript/main/typescript-logging-core.ts",
-
-  // Note commonjs is for jest as its not very good with esm modules.
   output: [
     {
       file: pkg.main,
@@ -20,17 +17,4 @@ export default {
       sourcemap: true,
     },
   ],
-
-  plugins: [
-    typescript({
-      typescript: require("typescript"),
-      clean: true,
-      tsconfig: "tsconfig.prod.json",
-    }),
-    commonjs(),
-    // Allow node_modules resolution, so you can use 'external' to control
-    // which external modules to include in the bundle
-    // https://github.com/rollup/rollup-plugin-node-resolve#usage
-    resolve(),
-  ],
-};
+}
---
+++ jest.config.base.js
@@ -0,0 +1,15 @@
+module.exports = {
+  "roots": [
+    "src/typescript",
+  ],
+  "preset": "ts-jest",
+  "testEnvironment": "jsdom",
+  "moduleFileExtensions": [
+    "ts",
+    "tsx",
+    "js",
+    "jsx",
+    "json",
+    "node",
+  ],
+};
