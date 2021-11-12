c9ea810a56a8fea21c6919c4f7635d7c5c17baaa
martijn <mreuvers@users.noreply.github.com>
2021-11-12 11:24:06 +0100

    Add initialize.sh
    Which cleans all node_modules and dist directories.
    
    Then cleanly installs all packages.

---
+++ initialize.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -e
+
+echo "Removing all node_modules directories and dist directories..."
+
+rm -rf node_modules dist
+rm -rf category-style/dist
+rm -rf log4ts-style/dist
+rm -rf tests-integration/rollup/node_modules
+rm -rf tests-integration/rollup/dist
+rm -rf tests-integration/webpack/node_modules
+rm -rf tests-integration/webpack/dist
+
+echo "Installing packages for core and style flavors..."
+npm ci
+
+echo ""
+echo "Installing packages for tests-integration"
+cd tests-integration/rollup
+npm ci
+cd ../webpack
+npm ci
+cd ../../
+
+echo ""
+echo "Installation success."
+echo "To build all logging and flavors:"
+echo "  npm run build --workspaces"
+echo "To build a test-integration project (requires previous command succeeded), go in respective directory (e.g. tests-integration/rollup):"
+echo "  npm run build"
+
