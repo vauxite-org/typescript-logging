ef4026e1ca4fef18af9764779958bb94ed1f6fca
martijn <mreuvers@users.noreply.github.com>
2023-03-08 19:53:49 +0100

    Issue #111: Upgrade rollup integration test dependencies
    Keeping cypress under version 10 though, as everything changed severely in 10
    and higher versions. Needs either a clean setup or a replacement of cypress later.

--- tests-integration/rollup/rollup.config.mjs
+++ tests-integration/rollup/rollup.config.mjs
@@ -1,6 +1,18 @@
 import typescript from "rollup-plugin-typescript2";
 import resolve from "@rollup/plugin-node-resolve";
 
+const tsConfigOptions = {
+  clean: true,
+  abortOnError: true,
+  tsconfig: "tsconfig.json",
+
+  tsconfigOverride: {
+    compilerOptions: {
+      noEmitOnError: true, // Rather have it in tsconfig, but atm this feature is ignored by the plugin.
+    },
+  },
+};
+
 export default {
   input: "src/typescript/main/index.ts",
   output: [
@@ -13,11 +25,7 @@ export default {
   ],
 
   plugins: [
-    typescript({
-      typescript: require("typescript"),
-      clean: true,
-      tsconfig: "tsconfig.json",
-    }),
+    typescript(tsConfigOptions),
     // commonjs(),
     // Allow node_modules resolution, so you can use 'external' to control
     // which external modules to include in the bundle
@@ -25,3 +33,4 @@ export default {
     resolve(),
   ],
 };
+
