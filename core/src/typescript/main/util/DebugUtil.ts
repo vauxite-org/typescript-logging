1773380beb37e1948b2a856c75dc1e3760ca4d27
martijn <mreuvers@users.noreply.github.com>
2023-03-08 20:01:14 +0100

    Issue #111: Drop unused file (was not compiling either anymore)

--- core/src/typescript/main/util/DebugUtil.ts
+++
@@ -1,6 +0,0 @@
-import {LogLevel} from "../core";
-import {CategoryConfig} from "../category";
-
-export function categoryConfigDebug(config: CategoryConfig) {
-  return `CategoryConfig=level: ${LogLevel[config.level].toString()}, allowSameCategoryName=${config.allowSameCategoryName}`;
-}
