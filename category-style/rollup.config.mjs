ba3f49d22a764f7faa61c109e38233556793d2f5
martijn <mreuvers@users.noreply.github.com>
2023-03-08 18:05:16 +0100

    Issue #111: Upgrade category-style dependencies to latest
    Same changes as core, with exception tsconfig.prod.json was already ok.
    Also the base files from core are used, so that simplifies this upgrade.

--- category-style/rollup.config.mjs
+++ category-style/rollup.config.mjs
@@ -1,8 +1,8 @@
-const sharedConfig = require("../core/rollup.config.base");
+import sharedConfig from "../core/rollup.config.base.mjs";
 
-import pkg from "./package.json";
+import pkg from "./package.json" assert {type: "json"};
 
-module.exports = {
+const SETTINGS = {
   ...sharedConfig,
   input: "src/typescript/main/typescript-logging-category.ts",
   output: [
@@ -18,3 +18,5 @@ module.exports = {
     },
   ],
 }
+
+export default SETTINGS;
